87b99c6374aec947ca1afa9fd25c09891d32b417
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2024-09-25 19:02:39 -0300

    Add files via upload

--- Tabelas.java
+++ Tabelas.java
@@ -1,6 +1,7 @@
 package br.com.tasks;
 
-public class Usuario {
+public class Tabelas {
+    // Atributos do Usuário
     private int idUsuario;
     private String nome;
     private String email;
@@ -8,7 +9,26 @@ public class Usuario {
     private String senha;
     private String criadoEm;
 
-    // Getters e Setters
+    // Atributos do Projeto
+    private int idProjeto;
+    private String nomeProjeto;
+    private String descricao;
+    private int criadoPor; // ID do usuário que criou o projeto
+    private String statusProjeto;
+    private String dataCriacao;
+    private String dataTermino;
+
+    // Atributos da Tarefa
+    private int idTarefa;
+    private String nomeTarefa;
+    private String descricaoTarefa;
+    private String statusTarefa;
+    private String criadoEmTarefa;
+    private int prioridade;
+    private int idProjetoTarefa; // ID do projeto relacionado
+    private int responsavel; // ID do usuário responsável
+
+    // Getters e Setters para Usuário
     public int getIdUsuario() {
         return idUsuario;
     }
@@ -55,5 +75,127 @@ public class Usuario {
 
     public void setCriadoEm(String criadoEm) {
         this.criadoEm = criadoEm;
+    }
+
+    // Getters e Setters para Projeto
+    public int getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(int idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+
+    public String getNomeProjeto() {
+        return nomeProjeto;
+    }
+
+    public void setNomeProjeto(String nomeProjeto) {
+        this.nomeProjeto = nomeProjeto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCriadoPor() {
+        return criadoPor;
+    }
+
+    public void setCriadoPor(int criadoPor) {
+        this.criadoPor = criadoPor;
+    }
+
+    public String getStatusProjeto() {
+        return statusProjeto;
+    }
+
+    public void setStatusProjeto(String statusProjeto) {
+        this.statusProjeto = statusProjeto;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(String dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+    // Getters e Setters para Tarefa
+    public int getIdTarefa() {
+        return idTarefa;
+    }
+
+    public void setIdTarefa(int idTarefa) {
+        this.idTarefa = idTarefa;
+    }
+
+    public String getNomeTarefa() {
+        return nomeTarefa;
+    }
+
+    public void setNomeTarefa(String nomeTarefa) {
+        this.nomeTarefa = nomeTarefa;
+    }
+
+    public String getDescricaoTarefa() {
+        return descricaoTarefa;
+    }
+
+    public void setDescricaoTarefa(String descricaoTarefa) {
+        this.descricaoTarefa = descricaoTarefa;
+    }
+
+    public String getStatusTarefa() {
+        return statusTarefa;
+    }
+
+    public void setStatusTarefa(String statusTarefa) {
+        this.statusTarefa = statusTarefa;
+    }
+
+    public String getCriadoEmTarefa() {
+        return criadoEmTarefa;
+    }
+
+    public void setCriadoEmTarefa(String criadoEmTarefa) {
+        this.criadoEmTarefa = criadoEmTarefa;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public int getIdProjetoTarefa() {
+        return idProjetoTarefa;
+    }
+
+    public void setIdProjetoTarefa(int idProjetoTarefa) {
+        this.idProjetoTarefa = idProjetoTarefa;
+    }
+
+    public int getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(int responsavel) {
+        this.responsavel = responsavel;
     }
 }
